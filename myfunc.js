c5e4165b6552c3209203d57c73d0b403693b0113
Rehre <akmalazzam2240@gmail.com>
2017-07-26 12:49:24 +0700

    fixing ig

--- myfunc.js
+++ myfunc.js
@@ -293,23 +293,58 @@ var stalkIg = function(keyword) {
       };
     } else {
       jsonIg = {
-        type: "template",
-        altText: "Stalk",
-        template: {
-          type: "carousel",
-          columns: [{
-            thumbnailImageUrl: profile_pic,
-            title: username,
-            text: deskripsi_profil,
-            actions: [{
-                type: "uri",
-                label: "Ke profil",
-                data: profile_url
-              }
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
             ]
-          }]
-        }
-      };
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
+          }
+      ]
+  }
+};
     }
     return jsonIg;
   }
@@ -318,7 +353,7 @@ var stalkIg = function(keyword) {
 // var data = stalkIg('lalala');
 //
 //
-// console.log(data.template.columns);
+// console.log(data.template.columns[0].actions);
 
 exports.stalkIg = stalkIg;
 exports.tekaTeki = tekaTeki;

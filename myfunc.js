4a5a4023dfcd29c4a9f55359379772f37b640b64
Rehre <akmalazzam2240@gmail.com>
2017-07-26 12:51:31 +0700

    fixing ig

--- myfunc.js
+++ myfunc.js
@@ -293,58 +293,33 @@ var stalkIg = function(keyword) {
       };
     } else {
       jsonIg = {
-  "type": "template",
-  "altText": "this is a carousel template",
-  "template": {
-      "type": "carousel",
-      "columns": [
-          {
-            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+        "type": "template",
+        "altText": "Stalk",
+        "template": {
+          "type": "carousel",
+          "columns": [{
+            "thumbnailImageUrl": profile_pic,
             "title": "this is menu",
             "text": "description",
-            "actions": [
-                {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=111"
-                },
-                {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=111"
-                },
-                {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/111"
-                }
+            "actions": [{
+                "type": "postback",
+                "label": "Buy",
+                "data": "action=buy&itemid=111"
+              },
+              {
+                "type": "postback",
+                "label": "Add to cart",
+                "data": "action=add&itemid=111"
+              },
+              {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/111"
+              }
             ]
-          },
-          {
-            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-            "title": "this is menu",
-            "text": "description",
-            "actions": [
-                {
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=222"
-                },
-                {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=222"
-                },
-                {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/222"
-                }
-            ]
-          }
-      ]
-  }
-};
+          }]
+        }
+      };
     }
     return jsonIg;
   }

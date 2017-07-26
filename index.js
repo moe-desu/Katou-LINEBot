0060a882fbf5e162a43bc3a767a8a10c8f5264a3
Rehre <akmalazzam2240@gmail.com>
2017-07-26 12:44:04 +0700

    fixing ig

--- index.js
+++ index.js
@@ -225,56 +225,7 @@ function handleEvent(event) {
         //katou stalk ig
         if (msgText.indexOf('Katou stalk') > -1) {
           var keyword = msgText.substr(12);
-          var objectIg = {
-            "type": "template",
-            "altText": "this is a carousel template",
-            "template": {
-              "type": "carousel",
-              "columns": [{
-                  "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-                  "title": "this is menu",
-                  "text": "description",
-                  "actions": [{
-                      "type": "postback",
-                      "label": "Buy",
-                      "data": "action=buy&itemid=111"
-                    },
-                    {
-                      "type": "postback",
-                      "label": "Add to cart",
-                      "data": "action=add&itemid=111"
-                    },
-                    {
-                      "type": "uri",
-                      "label": "View detail",
-                      "uri": "http://example.com/page/111"
-                    }
-                  ]
-                },
-                {
-                  "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-                  "title": "this is menu",
-                  "text": "description",
-                  "actions": [{
-                      "type": "postback",
-                      "label": "Buy",
-                      "data": "action=buy&itemid=222"
-                    },
-                    {
-                      "type": "postback",
-                      "label": "Add to cart",
-                      "data": "action=add&itemid=222"
-                    },
-                    {
-                      "type": "uri",
-                      "label": "View detail",
-                      "uri": "http://example.com/page/222"
-                    }
-                  ]
-                }
-              ]
-            }
-          };
+          var objectIg = myfunc.stalkIg(keyword);
           return client.replyMessage(token, objectIg);
         }
 

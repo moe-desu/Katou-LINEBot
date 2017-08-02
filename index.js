11a9a8f6e0a4319d1d68342f0b7d6e5242d0f0c2
Rehre <akmalazzam2240@gmail.com>
2017-08-02 20:13:46 +0700

    fixing osu profile 8

--- index.js
+++ index.js
@@ -392,8 +392,54 @@ function handleEvent(event) {
                   deskripsi_profil = "Level : " + Math.floor(parseInt(profile[0].level)) + "\nRank : " + profile[0].pp_rank + "\nTotal Score : " + profile[0].total_score + "\nPP :" + profile[0].pp_raw;
                   deskripsi_recent = "Judul : " + beatmap[0].title;
                   return client.replyMessage(token, {
-                    "type": "text",
-                    "text": deskripsi_profil
+                    "type": "template",
+                    "altText": "this is a carousel template",
+                    "template": {
+                      "type": "carousel",
+                      "columns": [{
+                          "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                          "title": "this is menu",
+                          "text": "description",
+                          "actions": [{
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=111"
+                            },
+                            {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=111"
+                            },
+                            {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/111"
+                            }
+                          ]
+                        },
+                        {
+                          "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                          "title": "this is menu",
+                          "text": "description",
+                          "actions": [{
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=222"
+                            },
+                            {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=222"
+                            },
+                            {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/222"
+                            }
+                          ]
+                        }
+                      ]
+                    }
                   });
                 });
               });

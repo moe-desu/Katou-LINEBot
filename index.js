4b907c0d438cea96aea5999fe4dc2d4b3bafd860
Rehre <akmalazzam2240@gmail.com>
2017-08-03 07:53:31 +0700

    fixing osu

--- index.js
+++ index.js
@@ -390,7 +390,7 @@ function handleEvent(event) {
                 myfunc.osuBeatmap(best[0].beatmap_id).then(function(hasil_beatmap) {
                   beatmap = hasil_beatmap;
                   deskripsi_profil = "Level : " + Math.floor(parseInt(profile[0].level)) + "    Acc : " + Math.floor(parseInt(profile[0].accuracy)) + "%\nRank : " + profile[0].pp_rank + "\nPP :" + profile[0].pp_raw;
-                  deskripsi_best = "Judul : " + beatmap[0].title + "\nScore : " + best[0].score + "\nCombo : " + best[0].maxcombo + "  PP : " + Math.floor(parseInt(best[0].pp));
+                  deskripsi_best = beatmap[0].title + "\nScore : " + best[0].score + "\nCombo : " + best[0].maxcombo + "  PP : " + Math.floor(parseInt(best[0].pp));
                   return client.replyMessage(token, {
                     "type": "template",
                     "altText": "Osu Profile",
